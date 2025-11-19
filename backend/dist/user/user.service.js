668db790275af32e5e99906b88f49f92b4ec08d6
Felipe Padovani <felipepadovani@rubeus.com.br>
2025-11-19 15:36:34 -0300

    fixing commits

--- backend/dist/user/user.service.js
+++ backend/dist/user/user.service.js
@@ -5,28 +5,85 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserService = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const user_entity_1 = require("./entities/user.entity");
+const typeorm_2 = require("typeorm");
 let UserService = class UserService {
-    create(createUserDto) {
-        return 'This action adds a new user';
+    userService;
+    constructor(userService) {
+        this.userService = userService;
     }
-    findAll() {
-        return `This action returns all user`;
+    async create(createUserDto) {
+        try {
+            const userData = {
+                name: createUserDto.name,
+                email: createUserDto.email,
+                passwordHash: createUserDto.password
+            };
+            const newUser = this.userService.create(userData);
+            return await this.userService.save(newUser);
+        }
+        catch (error) {
+            throw new common_1.HttpException(error, common_1.HttpStatus.BAD_REQUEST);
+        }
     }
-    findOne(id) {
-        return `This action returns a #${id} user`;
+    async findAll() {
+        const users = await this.userService.find({
+            order: {
+                id: 'desc'
+            }
+        });
+        return users;
     }
-    update(id, updateUserDto) {
-        return `This action updates a #${id} user`;
+    async findOne(id) {
+        const user = await this.userService.findOne({
+            where: {
+                id,
+            }
+        });
+        if (!user)
+            throw new common_1.NotFoundException('User not found');
+        return user;
     }
-    remove(id) {
-        return `This action removes a #${id} user`;
+    async update(id, updateUserDto) {
+        const partialUser = {
+            name: updateUserDto?.name
+        };
+        if (updateUserDto.password) {
+            partialUser['passwordHash'] = updateUserDto.password;
+        }
+        const personToUpdate = await this.userService.preload({
+            id,
+            ...partialUser
+        });
+        if (!personToUpdate)
+            throw new common_1.NotFoundException('User not found');
+        return this.userService.save(personToUpdate);
+    }
+    async remove(id) {
+        const user = await this.userService.findOne({
+            where: {
+                id,
+            }
+        });
+        if (!user)
+            throw new common_1.NotFoundException('User not found');
+        return this.userService.remove(user);
     }
 };
 exports.UserService = UserService;
 exports.UserService = UserService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __param(0, (0, typeorm_1.InjectRepository)(user_entity_1.User)),
+    __metadata("design:paramtypes", [typeorm_2.Repository])
 ], UserService);
 //# sourceMappingURL=user.service.js.map
